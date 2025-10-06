a3d485ebfa2941f76117e7a391cc06d80651bd38
Zhenxi33 <hemsocheat82@gmail.com>
2025-10-06 13:03:50 +0700

    second

--- array.cpp
+++ array.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Function to print array
+
 void printArray(int arr[], int size) {
     cout << "Array: ";
     for (int i = 0; i < size; i++) {
@@ -11,51 +11,41 @@ void printArray(int arr[], int size) {
     cout << endl;
 }
 
-// Function to insert element at given index
+
 int insertElement(int arr[], int size, int index, int value) {
-    // Shift elements to the right
+   
     for (int i = size; i > index; i--) {
         arr[i] = arr[i - 1];
     }
     
-    // Insert the new element
     arr[index] = value;
-    
-    // Return new size
     return size + 1;
 }
 
-// Function to delete element at given index
 int deleteElement(int arr[], int size, int index) {
-    // Shift elements to the left
     for (int i = index; i < size - 1; i++) {
         arr[i] = arr[i + 1];
     }
     
-    // Return new size
     return size - 1;
 }
 
 int main() {
-    int arr[100]; // Array with space for up to 100 elements
+    int arr[100]; 
     int size;
     
-    // Input array size
     cout << "Enter the number of elements in array: ";
     cin >> size;
     
-    // Input array elements
     cout << "Enter " << size << " elements:\n";
     for (int i = 0; i < size; i++) {
         cout << "Element " << i << ": ";
         cin >> arr[i];
     }
     
-    // Display original array
     cout << "\n";
     printArray(arr, size);
     
-    // Menu
     int choice;
     cout << "\nChoose operation:\n";
     cout << "1. Insert element\n";
@@ -64,19 +54,19 @@ int main() {
     cin >> choice;
     
     if (choice == 1) {
-        // Insert operation - add to end of array
+        
         int value;
         cout << "Enter value to insert: ";
         cin >> value;
         
-        arr[size] = value;  // Add to the end
+        arr[size] = value;  
         size++;
         
         cout << "\nAfter insertion:\n";
         printArray(arr, size);
         
     } else if (choice == 2) {
-        // Delete operation
+        
         int index;
         cout << "\nEnter index to delete (0 to " << size - 1 << "): ";
         cin >> index;
